1e8df232bfd2eaf70b4a9f39f21c8381fe04bc87
DESKTOP-O0JIHCH\ulun <b15145456@gmail.com>
2021-06-02 14:47:23 +0800

    member intialize

---
+++ memberInitialList/Sally.cpp
@@ -0,0 +1,14 @@
+#include "Sally.h"
+#include <iostream>
+using namespace std;
+
+Sally::Sally(int a, int b)
+: regVar(a), constVar(b)    //如果想要初始化一個const的變數
+{
+    
+}
+
+void Sally::print(){
+    cout << "regular var is: " << regVar << endl;
+    cout << "const variabel is : " << constVar << endl;
+}
---
+++ memberInitialList/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Sally.h"
+#include "Sally.cpp"
+using namespace std;
+int main()
+{
+    Sally so(3, 87);
+    so.print();
+}
